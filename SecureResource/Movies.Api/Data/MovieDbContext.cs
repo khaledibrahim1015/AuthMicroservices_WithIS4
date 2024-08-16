5d45d7b3c08a5e8df6854d97507c38511f7de494
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-08-17 01:17:58 +0300

    impl model impl context and seeddata

---
+++ SecureResource/Movies.Api/Data/MovieDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.Api.Models;
+
+namespace Movies.Api.Data;
+
+public class MovieDbContext : DbContext
+{
+    public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Movie> Movies { get; set; }
+
+}
