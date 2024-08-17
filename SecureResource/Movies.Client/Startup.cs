136e8b379aac2b4acf666c4269d7246333b4998e
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-08-17 16:30:14 +0300

    configure openid connect

---
+++ SecureResource/Movies.Client/Startup.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
+namespace Movies.Client
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllersWithViews();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+              .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+              .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+              {
+                  options.Authority = "https://localhost:5005";
+
+                  options.ClientId = "movies_mvc_client";
+                  options.ClientSecret = "secret";
+                  options.ResponseType = "code";
+
+                  options.Scope.Add("openid");
+                  options.Scope.Add("profile");
+
+
+
+                  options.SaveTokens = true;
+                  options.GetClaimsFromUserInfoEndpoint = true;
+
+
+              });
+
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+
+    }
+}
