2c1a463d02772eebcba623a2d0c34c99d4c3663d
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-08-17 12:04:36 +0300

    Configure IdentityServer with its Cofiguration

---
+++ SecureResource/IdentityServer/Startup.cs
@@ -0,0 +1,46 @@
+﻿namespace IdentityServer;
+
+public class Startup
+{
+    public IConfiguration Configuration;
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddIdentityServer()
+            .AddInMemoryClients(Config.Clients)
+            .AddInMemoryIdentityResources(Config.IdentityResources)
+            .AddInMemoryApiResources(Config.apiResources)
+            .AddInMemoryApiScopes(Config.ApiScopes)
+            .AddTestUsers(Config.TestUsers)
+            //.AddInMemoryPersistedGrants() // Adds in-memory store for persisted grants
+            .AddDeveloperSigningCredential();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+        app.UseIdentityServer();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("hello from identity");
+            });
+        });
+    }
+
+
+
+
+}
